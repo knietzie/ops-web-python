2221fc1907b92d186f991a49bb87d4fd7a646a94
Knietzie <knietzie@yahoo.com>
2015-08-26 17:13:25 +0800

    update put installation log messages

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,6 @@ Chef::Log.info("******Creating a data directory.******")
 
 data_dir = value_for_platform(
   "amazon" => { "default" => "/srv/www/shared" },
-  "ubuntu" => { "default" => "/srv/www/data" },
-  "default" => "/srv/www/config"
 )
 
 directory data_dir do
@@ -16,11 +14,18 @@ end
 
 Chef::Log.info("******Installing apache (httpd).******")
 package 'httpd' do
+  user: sudo
+  action :install
+end
+
+package 'httpd-devel' do
+  user: sudo
   action :install
 end
 
 Chef::Log.info("******Enable and start httpd.******")
 service 'httpd' do
+  user: sudo
   action [ :enable, :start ]
 end
 
@@ -29,3 +34,78 @@ cookbook_file '/var/www/html/index.html' do
    source 'index.html'
    mode '0644'
 end
+
+package "python27" do
+  user: sudo
+  action :install
+end
+
+package "python27-devel" do
+  user: sudo
+  action :install
+end
+
+package "gcc" do
+  user: sudo
+  action :install
+end
+
+
+package "gcc-c++" do
+  user: sudo
+  action :install
+end
+
+package "subversion" do
+  user: sudo
+  action :install
+end
+
+package "make" do
+  user: sudo
+  action :install
+end
+
+package "uuid" do
+  user: sudo
+  action :install
+end
+
+package "libuuid-devel" do
+  user: sudo
+  action :install
+end
+
+package "python-devel" do
+  user: sudo
+  action :install
+end
+
+package "python27-devel" do
+  user: sudo
+  action :install
+end
+
+package "make" do
+  user: sudo
+  action :install
+end
+
+package "postgresql93" do
+  user: sudo
+  action :install
+end
+
+package "postgresql93-devel" do
+  user: sudo
+  action :install
+end
+
+#yum install python27 python27-devel gcc gcc-c++ subversion git httpd make uuid libuuid-devel
+#yum install httpd-devel
+#ams apps requirements
+#yum install python-devel python27-devel nginx git make postgresql93 postgresql93-devel
+#yum install python27 python27-devel gcc gcc-c++ subversion git httpd make uuid libuuid-devel
+#yum install httpd-devel
+
+
