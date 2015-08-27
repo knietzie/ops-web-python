dd1ae69232a14e2bb83be31a5f2c288dc9c878e3
Knietzie <knietzie@yahoo.com>
2015-08-27 10:39:27 +0800

    add file index.html

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,12 +13,6 @@ directory data_dir do
   action :create
 end
 
-# Chef::Log.info("******Copying from index.html from cookbook.******")
-# cookbook_file '/var/www/html/index.html' do
-#    source 'index.html'
-#    mode '0644'
-# end
-
 bash 'update_and_install' do
   user "root"
   cwd "/tmp" 
@@ -27,6 +21,12 @@ bash 'update_and_install' do
     yum -y install httpd httpd-devel
     #yum install httpd httpd-devel python27 python27-devel gcc gcc-c++ subversion git httpd make uuid libuuid -devel install httpd-devel python-devel python27-devel nginx git make postgresql93 postgresql93-devel
     EOH
+end
+
+Chef::Log.info("******Copying from index.html from cookbook.******")
+cookbook_file '/var/www/html/index.html' do
+   source 'index.html'
+   mode '0644'
 end
 
 Chef::Log.info("******Enable and start httpd.******")
