fb37cd5622119f4c370f6edf5e9ca749e9eabf43
Knietzie <knietzie@yahoo.com>
2015-09-04 10:32:50 +0800

    u6

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,11 +1,43 @@
-git "ams" do
-  repository "git@bitbucket.org:imfree/ams.git"
-  revision "testing"
-  action :sync
-  destination "/srv/www/shared"
-  user "root"
-  #ssh_wrapper "/tmp/private_code/wrap-ssh4git.sh"
+# git "ams" do
+#   repository "git@bitbucket.org:imfree/ams.git"
+#   revision "testing"
+#   action :sync
+#   destination "/srv/www/shared"
+#   user "root"
+
+#   # add id_rsa (private)
+#   # test bitbucket conn  ssh -Tv git@bitbucket.org
+#   # git ls-remote "git@bitbucket.org:imfree/ams.git" testing
+#   # sudo opsworks-agent-cli run_command
+#   #ssh_wrapper "/tmp/private_code/wrap-ssh4git.sh"
+#   # deploy wrapper http://stackoverflow.com/questions/18388722/chef-deploy-resource-private-repo-ssh-deploy-keys-and-ssh-wrapper
+#   #http://www.jasongrimes.org/2012/06/deploying-a-lamp-application-with-chef-vagrant-and-ec2-3-of-3/#private-repo
+
+
+# end
+###-------------------###
+
+#include_recipe "hello_app::webserver" 
+
+deploy_revision node['ams']['deploy_dir'] do
+  scm_provider Chef::Provider::Git 
+  repo node['ams']['deploy_repo']
+  revision node['ams']['deploy_branch']
+  enable_submodules true
+  shallow_clone false
+  symlink_before_migrate({}) # Symlinks to add before running db migrations
+  purge_before_symlink [] # Directories to delete before adding symlinks
+  create_dirs_before_symlink ["config"] # Directories to create before adding symlinks
+  ###symlinks({"config/local.config.php" => "config/local.config.php"})
+  # migrate true
+  # migration_command "php app/console doctrine:migrations:migrate" 
+  action :deploy
+  
+  restart_command do
+    service "httpd" do action :restart; end
+  end
 end
+
 
 # deploy "#{node[:app][:dir]}" do 
 #     repo "#{node[:app][:repo]}"
