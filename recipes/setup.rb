2c7840d006a1ee4a4a7b8003b7aacd4aaa5f000e
Knietzie <knietzie@yahoo.com>
2015-09-06 16:31:25 +0800

    u13

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,9 +1,10 @@
 Chef::Log.info("****** Setup deployment directory ******")
 data_dir = value_for_platform(
-  "amazon" => { "default" => "/srv/www/shared" },
-  "centos" => { "default" => "/srv/www/shared" }
+  "amazon" => { "default" => "/srv/www" },
+  "centos" => { "default" => "/srv/www" }
 )
 
+# Create deployment directory
 directory data_dir do
   mode 0755
   owner 'root'
@@ -12,95 +13,64 @@ directory data_dir do
   action :create
 end
 
+# Create default python logs - later to modify 
+# touch /opt/python/log/mobilecrashlogs
+log_dir = "/opt/python/log"
+directory log_dir do
+  mode 0755
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+end  
+
+#Creare mobilecrashlogs file
+execute "create_mobilecrashlogs" do
+    command "sudo touch /opt/python/log/mobilecrashlog"
+end  
+
 
 Chef::Log.info("****** Installing container applications ******")
 bash 'install' do
   user "root"
   cwd "/tmp" 
   code <<-EOH
-    #yum update 
-    yum -y install httpd httpd-devel python python-devel python27-devel make git gcc gcc-c++ uuid libuuid-devel mysql-devel postgresql93 postgresql93-devel postgresql-devel
+    yum update 
+    yum -y install python27 python27-devel make git gcc gcc-c++ uuid libuuid-devel mysql-devel postgresql93 postgresql93-devel postgresql-devel
     EOH
 end
 
-# Chef::Log.info("****** Python 2.7 from tarball ******")
-# bash 'install' do
-#   user "root"
-#   cwd "/tmp" 
-#   code <<-EOH   
-#     wget https://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz
-#     tar xvfz Python-2.7.8.tgz
-#     cd Python-2.7.8
-#     ./configure --prefix=/usr/local
-#     make 
-#     make altinstall
-#     EOH
-# end
-
-# execute "install EPEL" do
-#     command "rpm -i --force http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
-# end
-
+#******* Enable this in AWS Production **********
 execute "install_EPEL" do
-    command "rpm -qa | grep -qw  epel-release-6-8.noarch | rpm -i --force http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
-    # command "yum -y install http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+  command "rpm -qa | grep -qw  epel-release-6-8.noarch | rpm -i --force http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+#     # command "yum -y install http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
 end
+#******* Enable this in AWS Production **********
 
-execute "install_python-pip" do
-    command "yum -y install python-pip"
+
+####### Using NGINX Install nginx ------
+# sudo yum install epel-release
+# https://www.digitalocean.com/community/tutorials/how-to-deploy-python-wsgi-apps-using-gunicorn-http-server-behind-nginx
+
+
+# Install nginx
+execute "Install_nginx" do
+  command "yum -y install nginx"
+do
+
+# start nginx
+execute "start_nginx" do
+  command "sudo service nginx start"
 end
-
-# Gaining Access to SCLs
-# https://wiki.centos.org/AdditionalResources/Repositories/SCL
-execute "install_SCL" do
-    command "yum -y install python27"
-end
-
-execute "install_mod_wsgi" do
-    command "yum -y install mod_wsgi"
-end  
-
-execute "Load_mod_wsgi module" do
-    command "echo 'LoadModule wsgi_module modules/mod_wsgi.so' > /etc/httpd/conf.d/wsgi.conf"
-end    
-
-#Use WSGI.conf.erb template
-template "/etc/httpd/conf.d/wsgi.conf" do
-  source "wsgi.conf.erb"
-end
+####### Using NGINX Install nginx ------
 
 
-# execute "wsgi.conf" do
-#      command "echo 
-#      'WSGIScriptAlias / /srv/www/current/ams/wsgi.py
-#      WSGIPythonPath /srv/wwww/current
-
-#      <Directory /srv/www/current>
-#      <Files wsgi.py>
-#      Require all granted#
-#       </Files>
-#      </Directory>' > /etc/httpd/conf.d/wsgi.conf"
-#  end
+# execute "install_pip" do
+#     command "yum -y install python-pip"
+# end  
 
 
-
-# Chef::Log.info("******Copying from index.html to replace standard http welcome pag.******")
-# cookbook_file '/var/www/html/index.py' do
-#    source 'index.py'
-#    mode '0644'
-# end
-
-Chef::Log.info("****** Start Httpd ******")
-service 'httpd' do
-  action [ :enable, :start ]
-end
-
-# Install virtualenv via Pip
-# virtualenv venv
-# source venv/bin/activate
-#
-
-execute "install_package" do
+execute "install_virtualenv" do
     command "sudo pip install virtualenv"
 end  
 
@@ -110,13 +80,14 @@ bash 'activate_virtualenv' do
   cwd "/tmp" 
   code <<-EOH
     cd /srv/www
-    virtualenv venv
+    virtualenv venv --python=/usr/local/bin/python
     source venv/bin/activate
-    #venv/bin/pip install -r current/requirements.txt
+    venv/bin/pip install --upgrade pip
+    venv/bin/pip install gunicorn
   EOH
 end
 
-# # Install python2.7 via venv
+# Install python2.7 via venv
 # bash 'install_python2.7' do
 #   user "root"
 #   cwd "/tmp"
@@ -130,21 +101,20 @@ Chef::Log.info("****** --------------- Start of Deployment -----------------****
 
 
 # Initial deploy
-bash 'deploy' do
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-    /srv/www/venv/bin/easy_install install python2.7
-  EOH
-end 
+# bash 'deploy' do
+#   user "root"
+#   cwd "/tmp"
+#   code <<-EOH
+#     /srv/www/venv/bin/easy_install install python2.7
+#   EOH
+# end 
 
 
 Chef::Log.info("****** --------------- Pull From Repo -----------------******")
 # - Pull from repo
 # - Set symnlinks
-#   git clone -b develop git@github.com:user/myproject.git
-
-#
+#git clone -b develop git@github.com:user/myproject.git
+# http://www.rootdown.net/blog/2012/04/10/introducing-deploy-wrapper/
 # After pullout, go to virtualenv
 # Install requirements.txt
 # Restart httpd
@@ -156,7 +126,7 @@ Chef::Log.info("****** --------------- Pull From Repo -----------------******")
 
 # Notes on installing python, it should be 2.7 to avoid compatibility inssues
 # http://tuxlabs.com/?p=194
-
+#
 
 
 
@@ -167,10 +137,17 @@ Chef::Log.info("****** --------------- Run DBMigrate  -----------------******")
 #3
 #4
 
+# deploy "#{node[:app][:dir]}" do
+#     repo "#{node[:app][:repo]}"
+#     revision "#{node[:app][:branch]}"
+#     user "deploy"
+#     #owner "deploy
+#     purge_before_symlink %w{tmp log}
+#     create_dirs_before_symlink ["tmp"]
+#     #symlink_before_migrate "system/database.js" => "config/database-sim.js"
+#     symlinks "pids"=>"tmp/pids", "log"=>"log"
+
+# end
 Chef::Log.info("****** --------------- Update Symlink -----------------******")
-
-
-
-
 
 
