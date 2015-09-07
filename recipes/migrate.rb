832b2076bd91bba8ec3ac7e6f7c10e4e9cb8756c
Knietzie <knietzie@yahoo.com>
2015-09-07 14:27:45 +0800

    create migrate.rb

---
+++ recipes/migrate.rb
@@ -0,0 +1,19 @@
+Chef::Log.info("****** Run Migrations ******") 
+
+bash 'Run migrations' do
+  user "root"
+  cwd "/tmp" 
+  code <<-EOH
+    cd /srv/www
+    source venv/bin/activate
+    cd /srv/www/current
+    ## 01_syncdb:
+    ./manage.py syncdb --noinput
+    ## 03_collectstatic:
+    ## 04_wsgipass:
+    ## 05_migrations:
+    ./manage.py migrate --noinput 
+    ## 06_data:
+    ./manage.py loaddata city_data.json barangay_data.json language_data.json error_message_data.json error_translation_data.json
+  EOH
+end
--- recipes/setup_webserver.rb
+++
@@ -1,7 +0,0 @@
-Chef::Log.info("****** Install and starting apache2 web server ******")
-service 'apache2' do
-  supports :restart => true, :reload => true, :status => true, :enable => true, :reload => true 
-  action :install 
-end
-
-
